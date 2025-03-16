342dcebda5157bfb2b2a7bb793cc14f0d1a200a6
Mushkan23 <mushkansingh733@gmail.com>
2025-03-16 23:45:47 +0530

    Updated the supabase.js file

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,11 +1,20 @@
-
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;  //Exported
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseClient = async (supabaseAccessToken) => {
+  const supabase = createClient(supabaseUrl, supabaseKey,{
+    global:{
+      headers:{
+        Authorization: `Bearer ${supabaseAccessToken}`,
+      },
+    },
+  });
 
-export default supabase
+  return supabase;
+};
+
+export default supabaseClient;
         
 
 // set Supabase JWT on the client object,
